bab3e3565eaaa53c4b60802d29cfa1c5dbcf4210
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-11-19 20:41:09 +0100

    Implement component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React,{Component} from 'react'
 
-const SearchBar = ()=>{
-    return <input />
+class SearchBar extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {term:''};
+    }
+    render(){
+        return (
+            <div>
+            <input 
+                value={this.state.term}
+                onChange={event=> this.setState({term:event.target.value})} 
+            />
+            Value is : <p>{this.state.term}</p>
+            </div>
+        )
+    }
 }
-
 export default SearchBar
